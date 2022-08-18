cf69716eff3be17d2f16401564bd7cc364721748
pratheeshraj <110892110+pratheeshraj@users.noreply.github.com>
2022-08-18 16:52:15 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,16 @@
+let resume ={
+   Name:"Pratheesh.N",
+   Qualification:"BE",
+   Department:"Mechanical",
+   Collage:"SCAD collage of Engineering and Technology",
+   Address:"veeyanoor, kannya kumari"
+
+
+}
+
+
+let JSONFORM=JSON.stringify(resume);
+console.log(JSONFORM)
+
+
+
